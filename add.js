baa66cbd2005eb7395cf6e6a109d696b65495083
jackoff96 <amankachhap673@gmail.com>
2024-03-26 13:50:46 +0530

    hello theis is the

--- add.js
+++ add.js
@@ -1,3 +1,1 @@
-console.log("hello world ");
-document.write("this is the never that should be done on the basis of the file of the and the folder that should be done");
 alert("this is the one for the file that shouuld be done on the basis of the folder");
