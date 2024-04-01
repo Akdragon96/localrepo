7165fd9dc8cb8af363bafe2a9625b2171a9a3d31
jackoff96 <amankachhap673@gmail.com>
2024-04-01 13:44:50 +0530

    direct commited

--- addon.js
+++ addon.js
@@ -1,1 +1,6 @@
 alert("HELLO WORLD THIS IS THE FIRST FILE IN THE MORNING THAT SHOULD BE DONE");
+document.write("lets write make the file that is used to add two numbers that shouhld  ");
+let a=9;
+let b=7;
+document.write("The sum of the two number are :",a+b);
+alert("hello world for this is the while loop");
